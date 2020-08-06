184c0975898e40c52c78faa11f9b260e0dab9244
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-08-06 15:02:49 +0100

    add dark mode using vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,45 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Initial State
+const userSelectedDarkMode = window.localStorage.getItem("isDarkMode") === true;
+const state = {
+  isDarkMode: userSelectedDarkMode,
+};
+
+// Getters
+const getters = {
+  isDarkMode(state) {
+    return state.isDarkMode;
+  },
+};
+
+// Mutations
+const mutations = {
+  toggleDarkMode(state) {
+    if (state.isDarkMode === true) {
+      state.isDarkMode = false;
+      document.body.style.backgroundColor = "#f0f3f5";
+      window.localStorage.setItem("isDarkMode", "false");
+    } else {
+      state.isDarkMode = true;
+      document.body.style.backgroundColor = "#212c4f";
+      window.localStorage.setItem("isDarkMode", "true");
+    }
+  },
+};
+
+// Actions
+const actions = {
+  triggerDarkMode(context) {
+    context.commit("toggleDarkMode");
+  },
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: {},
 });
