71314d0b8ef8c3aab3a609e64a9a97987e38ddb3
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-08-06 12:40:52 +0100

    add more routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home";
 import About from "../views/About";
+import SignIn from "../views/Auth/SignIn";
+import Recover from "../views/Auth/Recover";
+import Request from "../views/Auth/Request";
 
 Vue.use(VueRouter);
 
@@ -16,12 +19,27 @@ const routes = [
     name: "About",
     component: About,
   },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/request",
+    name: "Request",
+    component: Request,
+  },
+  {
+    path: "/recover",
+    name: "Recover",
+    component: Recover,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
