cecafb43b41240b9c09b8a9d16e8e6b24a49ec8f
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-08-06 16:47:18 +0100

    add team markup

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -7,31 +7,61 @@
         appear
         appear-active-class="animate__animated animate__flipInX"
       >
-        <a href="" class="card card-1"> </a
+        <a
+          href=""
+          class="card card-1"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        >
+          <img src="@/assets/slack.png" alt="slack" class="card-header" />
+          <h3>Slack</h3>
+          <p :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+            Messaging app for all team communication
+          </p>
+        </a></transition
+      >
+      <transition
+        appear
+        appear-active-class="animate__animated animate__flipInX"
+      >
+        <a
+          href=""
+          class="card card-2"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        >
+        </a
       ></transition>
       <transition
         appear
         appear-active-class="animate__animated animate__flipInX"
       >
-        <a href="" class="card card-2"> </a
+        <a
+          href=""
+          class="card card-3"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        >
+        </a
       ></transition>
       <transition
         appear
         appear-active-class="animate__animated animate__flipInX"
       >
-        <a href="" class="card card-3"> </a
+        <a
+          href=""
+          class="card card-4"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        >
+        </a
       ></transition>
       <transition
         appear
         appear-active-class="animate__animated animate__flipInX"
       >
-        <a href="" class="card card-4"> </a
-      ></transition>
-      <transition
-        appear
-        appear-active-class="animate__animated animate__flipInX"
-      >
-        <a href="" class="card card-5"> </a
+        <a
+          href=""
+          class="card card-5"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        >
+        </a
       ></transition>
     </div>
   </div>
@@ -40,6 +70,11 @@
 <script>
 export default {
   name: "Team",
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    },
+  },
 };
 </script>
 
@@ -60,10 +95,11 @@ h2 {
 }
 
 .card {
-  height: 50px;
-  width: 50px;
-  background: red;
+  height: 400px;
+  width: 100%;
+  max-width: 300px;
   margin: 20px;
+  border-radius: 10px;
 }
 
 .card-1 {
@@ -80,5 +116,41 @@ h2 {
 }
 .card-5 {
   animation-delay: 2s;
+}
+
+.card-header {
+  width: 100%;
+  border-radius: 10px 10px 0 0;
+}
+
+h3 {
+  @include heading-3;
+  margin-bottom: 16px;
+}
+
+p {
+  font-size: 15px;
+  line-height: 24px;
+  text-align: left;
+  margin-left: 16px;
+  margin-top: 0;
+}
+
+.light-card {
+  background: $white;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.15);
+
+  &:hover {
+    box-shadow: 0px 30px 60px rgba(0, 0, 0, 0.35);
+  }
+}
+
+.dark-card {
+  background: $super-dark-blue;
+  box-shadow: 0px 20px 40px rgba(255, 255, 255, 0.15);
+
+  &:hover {
+    box-shadow: 0px 30px 60px rgba(255, 255, 255, 0.35);
+  }
 }
 </style>
